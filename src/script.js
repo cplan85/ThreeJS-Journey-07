af216bc4d85ca16a5d761915e8a1236b5af6a14f
cplan85 <carlos_planchart@live.com>
2023-01-11 23:25:33 +0100

    added orbit controls

--- src/script.js
+++ src/script.js
@@ -1,5 +1,6 @@
 import './style.css'
 import * as THREE from 'three'
+import { OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js'
 
 /**
  * Cursor
@@ -48,6 +49,12 @@ console.log(camera.position.length())
 camera.lookAt(mesh.position)
 scene.add(camera)
 
+//Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true;
+//Change the target y axis.
+// controls.target.y = 1
+// controls.update()
 // Renderer
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
@@ -65,11 +72,14 @@ const tick = () =>
   //  mesh.rotation.y = elapsedTime;
 
   //Update camera
-  camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3
-  camera.position.y = cursor.y * 5
-  camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3
-  camera.lookAt(mesh.position)
+//   camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3
+//   camera.position.y = cursor.y * 5
+//   camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3
+//   camera.lookAt(mesh.position)
     // Render
+
+    //Update controls
+    controls.update()
     renderer.render(scene, camera)
 
     // Call tick again on the next frame
